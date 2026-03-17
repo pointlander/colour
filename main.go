fa12624b756911e2a0080eb084bfff12d28a7ecb
Andrew Snodgrass <pointlander@gmail.com>
2026-03-17 16:49:18 -0500

    Copy dct and pagerank code

--- main.go
+++ main.go
@@ -5,8 +5,54 @@
 package main
 
 import (
+	"math"
+
 	"gitlab.com/gomidi/midi/writer"
 )
+
+const (
+	N int = 8
+)
+
+var (
+	c, cT [N][N]float64
+)
+
+func init() {
+	for j := 0; j < N; j++ {
+		c[0][j] = 1.0 / math.Sqrt(float64(N))
+		cT[j][0] = c[0][j]
+	}
+
+	for i := 1; i < N; i++ {
+		for j := 0; j < N; j++ {
+			jj, ii := float64(j), float64(i)
+			c[i][j] = math.Sqrt(2.0/8.0) * math.Cos(((2.0*jj+1.0)*ii*math.Pi)/(2.0*8.0))
+			cT[j][i] = c[i][j]
+		}
+	}
+}
+
+func ForwardDCT(in *[N][N]uint8, out *[N][N]float64) {
+	var x [N][N]float64
+	for i := 0; i < N; i++ {
+		for j := 0; j < N; j++ {
+			for k := 0; k < N; k++ {
+				x[i][j] += float64(int(in[i][k])-128) * cT[k][j]
+			}
+		}
+	}
+
+	for i := 0; i < N; i++ {
+		for j := 0; j < N; j++ {
+			y := 0.0
+			for k := 0; k < N; k++ {
+				y += c[i][k] * x[k][j]
+			}
+			out[i][j] = y
+		}
+	}
+}
 
 func main() {
 	err := writer.WriteSMF("notes.mid", 1, func(wr *writer.SMF) error {
